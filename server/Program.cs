dcc826b9c5c3d40aac095a6c8c0ee92aafba7470
yilmazoncum <yilmazoncum@gmail.com>
2024-01-26 15:56:10 +0300

    add request-response

---
+++ server/Program.cs
@@ -0,0 +1,21 @@
+//Server side
+
+var app = WebApplication.CreateBuilder(args).Build();
+
+var cities = new[]
+{
+    "New York", "Amsterdam", "Berlin","Moscow","Rome",
+    "Istanbul","London","Paris","Tokyo","Madrid"
+};
+
+app.MapGet("/city", () => {
+    
+    //generate random number
+    Random r = new Random();   
+    int randomNumber = r.Next(0,10); 
+    Console.WriteLine(randomNumber);
+
+    return cities[randomNumber];
+});
+
+app.Run();
